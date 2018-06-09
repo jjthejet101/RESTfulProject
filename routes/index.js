17d37179979984cd6b2c061eaf4cdd528f5d0181
Unknown <justinkjetton@gmail.com>
2018-06-08 20:55:57 -0500

    required dependencies

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+/* required dependencies */ 
+const sqlite3 = require('sqlite3').verbose(); 
+const Sequelize = require('Sequelize'); 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
