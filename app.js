abbf507b09afe213a6368948d5e010c135ba6a68
Unknown <justinkjetton@gmail.com>
2018-06-08 22:39:34 -0500

    comments

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+// beginning of Pipeline
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,6 +24,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
+// an example of Middleware
 app.use(function(req, res, next) {
   next(createError(404));
 });
@@ -37,5 +39,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+// end of Pipeline
 
 module.exports = app;
