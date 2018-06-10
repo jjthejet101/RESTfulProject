b79a55884ae0440f1f12aa16aa8ef63c1bf593d3
Unknown <justinkjetton@gmail.com>
2018-06-09 23:16:58 -0500

    3.1

--- routes/index.js
+++ routes/index.js
@@ -41,20 +41,31 @@ const Artist = sequelize.define(
 //   console.log(myTableRows);
 // });
 
-/* GET Albums */
+// /* GET Albums and Album-Artist */
+// router.get('/', function(req, res, next) {
+//   let queryAlbum = req.query.musicRequest;
+//   if (queryAlbum == "albums") {
+//     sequelize.query("SELECT Title FROM Album").then(myTableRows => {
+//       res.send(myTableRows);
+//     });
+//   } else if (queryAlbum == "album-artist") {
+//     sequelize.query(`SELECT Artist.Name as Artist, Album.Title as Album FROM Artist JOIN Album WHERE Artist.ArtistId=Album.ArtistId LIMIT 1000`).then(myTableRows => {
+//       res.send(myTableRows);
+//     });
+//   } else {
+//     res.render('error', { message: '404 - Not Found: Could not find the ' + queryAlbum + ' table in the database.' });
+//   };
+// });
+
+/* GET Albums and Album-Artist */
 router.get('/', function(req, res, next) {
-  let queryAlbum = req.query.musicRequest;
-  if (queryAlbum == "albums") {
-    sequelize.query("SELECT Title FROM Album").then(myTableRows => {
-      res.send(myTableRows);
-    });
-  } else if (queryAlbum == "album-artist") {
-    sequelize.query(`SELECT Artist.Name as Artist, Album.Title as Album FROM Artist JOIN Album WHERE Artist.ArtistId=Album.ArtistId LIMIT 1000`).then(myTableRows => {
-      res.send(myTableRows);
-    });
-  } else {
-    res.render('error', { message: '404 - Not Found: Could not find the ' + queryAlbum + ' table in the database.' });
-  };
+  let queryMinutes = req.query.musicTrackMinutes;
+  let querySeconds = req.query.musicTrackSeconds;
+  let millisecs = (parseInt(queryMinutes) * 60 + parseInt(querySeconds)) * 1000;
+  let query = `SELECT Name, Milliseconds FROM Track WHERE Milliseconds > ${millisecs} LIMIT 1000`;
+  sequelize.query(query).then(myTableRows => {
+    res.send(myTableRows);
+  });
 });
 
 module.exports = router;
