2c78d367552f1951e47ee5b92fe63dbfa5f656e5
Unknown <justinkjetton@gmail.com>
2018-06-09 22:30:29 -0500

    1.6

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var createError = require('http-errors');
 
 /* required dependencies */ 
 const sqlite3 = require('sqlite3').verbose(); 
@@ -49,6 +50,8 @@ router.get('/', function(req, res, next) {
     sequelize.query("SELECT Title FROM Album").then(myTableRows => {
       res.send(myTableRows);
     });
+  } else {
+    res.render('error', { message: '404 - Not Found: Could not find the ' + queryAlbum + ' table in the database.' });
   };
 });
 
